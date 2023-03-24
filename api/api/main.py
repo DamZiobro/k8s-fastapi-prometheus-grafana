cced00e22eb03fad30839f70848e74f896f08286
Damian Ziobro <damian@xmementoit.com>
2023-03-24 12:23:00 +0000

    add Prometheus-based instrumentation to the API

--- api/api/main.py
+++ api/api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+instrumentator = Instrumentator().instrument(app)
+
+
+# it will generate /metrics endpoint
+@app.on_event("startup")
+async def _startup():
+    instrumentator.expose(app)
+
 
 @app.get("/")
-async def root():
+async def read_root():
     return {"message": "Hello World"}
